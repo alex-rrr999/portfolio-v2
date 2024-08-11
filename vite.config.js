db43c480b2161d663df7152d6d54f77e1348df77
alex-rrr999 <rodrialex.edu@gmail.com>
2024-08-11 14:25:28 -0700

    attempt 5

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,25 @@
-const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
+import copy from 'vite-plugin-copy';
+
+const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env;
 
 export default {
     root: 'src/',
     publicDir: '../static/',
     base: './',
-    server:
-    {
+    server: {
         host: true,
         open: !isCodeSandbox // Open if it's not a CodeSandbox
     },
-    build:
-    {
+    build: {
         outDir: '../dist',
         emptyOutDir: true,
         sourcemap: true
-    }
+    },
+    plugins: [
+        copy({
+            targets: [
+                { src: '.well-known', dest: '../dist' }
+            ]
+        })
+    ]
 }
