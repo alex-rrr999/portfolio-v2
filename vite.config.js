df92f78db37bd908eeac64d8b8e69b2dde614be3
alex-rrr999 <rodrialex.edu@gmail.com>
2024-08-11 14:39:34 -0700

    final attempt

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,22 @@
-import { defineConfig } from 'vite';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
 
-const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env;
 
-export default defineConfig({
+export default {
     root: 'src/',
     publicDir: '../static/',
     base: './',
-    server: {
+    server:
+    {
         host: true,
         open: !isCodeSandbox // Open if it's not a CodeSandbox
     },
-    build: {
+    build:
+    {
         outDir: '../dist',
         emptyOutDir: true,
         sourcemap: true
-    },
-    plugins: [
-        viteStaticCopy({
-            targets: [
-                {
-                    src: '../static/.well-known/', // Replace with the path to the folder you want to copy
-                    dest: '' // Destination within the dist directory
-                }
-            ]
-        })
-    ]
-});
+    }
+
+
+    
+}
